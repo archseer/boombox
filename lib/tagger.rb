60969b5634b1edd37c4a97aa866f58634ff9b28f
Blaž Hrastnik <speed.the.bboy@gmail.com>
2012-11-06 22:25:22 +0100

    Split Tagger into a separate file.

---
+++ lib/tagger.rb
@@ -0,0 +1,69 @@
+module Tagger
+  def self.generate_db sinatra
+    files = Dir["#{File.dirname(__FILE__)}/../public/music/**/*.mp3"]
+    files.each {|filename|
+      file = TagLib::MPEG::File.new filename
+      tag = file.id3v2_tag
+      old_tag = file.id3v1_tag
+
+      disc = tag.frame_list('TPOS').first 
+      disc = disc ? disc.to_s.split('/') : [nil, nil]
+
+      t = Track.new(
+        :title => tag.title || old_tag.title || File.basename(filename).gsub(/\.mp3\z/, ''),
+        :artist => tag.artist || old_tag.artist,
+        :album => tag.album || old_tag.album,
+        :year => tag.year || old_tag.year,
+        :track => tag.track || old_tag.track,
+        :time => file.audio_properties.length,
+        :disc => disc[0],
+        :albumartist => tag.frame_list('TPE2').first,
+        :total_tracks => tag.frame_list('TRCK').first.to_s.split('/')[1],
+        :total_discs => disc[1],
+        :genre => tag.genre || old_tag.genre,
+        #:rating => ,
+        :bpm => tag.frame_list('TBPM').first,
+      )
+      t.filename = sinatra.relative_to filename
+      t.save!
+    }
+  end
+
+  def self.write_tags track
+    # Load an ID3v2 tag from a file
+    TagLib::MPEG::File.open("public/#{track.filename}") do |file|
+      tag = file.id3v2_tag
+
+      tag.title = track.title
+      tag.artist = track.artist
+      tag.album = track.album
+      tag.year = track.year.to_i
+      tag.genre = track.genre
+      # track count
+      if track.total_tracks and track.track
+        tag.frame_list('TRCK').first.text = "#{track.track}/#{track.total_tracks}"
+      elsif tag.track
+        tag.track = track.track.to_i
+      end
+      # add album artist
+      if track.albumartist
+        tag.add_frame TagLib::ID3v2::TextIdentificationFrame.new('TPE2',TagLib::String::UTF8) if tag.frame_list('TPE2').empty?
+        tag.frame_list('TPE2').first.text = track.albumartist
+      end
+      # add disc
+      if track.disc
+        tag.add_frame TagLib::ID3v2::TextIdentificationFrame.new('TPOS',TagLib::String::UTF8) if tag.frame_list('TPOS').empty?
+        disc = track.disc.to_s
+        disc << "/#{track.total_discs}" if track.total_discs
+        tag.frame_list('TPOS').first.text = disc
+      end
+      # bpm
+      if track.bpm
+        tag.add_frame TagLib::ID3v2::TextIdentificationFrame.new('TBPM',TagLib::String::UTF8) if tag.frame_list('TBPM').empty?
+        tag.frame_list('TBPM').first.text = track.bpm.to_s
+      end
+
+      file.save
+    end
+  end
+end
--- web_interface.rb
+++ web_interface.rb
@@ -9,6 +9,8 @@ require 'taglib'
 require 'pathname'
 require 'json'
 
+require_relative 'lib/tagger'
+
 # disable running by default, otherwise just requiring it would trigger a duplicate
 set :run, false
 
@@ -103,7 +105,7 @@ class WebInterface < Sinatra::Base
     if ids.length > 1
       tag.delete_keys 'title' # delete per-track specific title
       # delete any keys that aren't checkmarked
-      tag.delete_if {|key, value| !(params[:check].include? key) }
+      tag.delete_if {|key, value| !params[:check].include? key }
     end
 
     ids.each {|id| 
@@ -122,7 +124,7 @@ class WebInterface < Sinatra::Base
     if params[:id] != "undefined"
       track = Track.find params[:id]
       if track
-        body json :album => track.album,:artist => track.artist,:title => track.title, :cover => track.cover
+        body json :album => track.album, :artist => track.artist, :title => track.title, :cover => track.cover
       else
         body json :title => "Track not found."
       end
@@ -142,74 +144,3 @@ class WebInterface < Sinatra::Base
 
 end
 
-#---------------------------------
-
-class Tagger
-  def self.generate_db sinatra
-    files = Dir["#{File.dirname(__FILE__)}/public/music/**/*.mp3"]
-    files.each {|filename|
-      file = TagLib::MPEG::File.new filename
-      tag = file.id3v2_tag
-      old_tag = file.id3v1_tag
-
-      disc = tag.frame_list('TPOS').first 
-      disc = disc ? disc.to_s.split('/') : [nil, nil]
-
-      t = Track.new(
-        :title => tag.title || old_tag.title || File.basename(filename).gsub(/\.mp3\z/, ''),
-        :artist => tag.artist || old_tag.artist,
-        :album => tag.album || old_tag.album,
-        :year => tag.year || old_tag.year,
-        :track => tag.track || old_tag.track,
-        :time => file.audio_properties.length,
-        :disc => disc[0],
-        :albumartist => tag.frame_list('TPE2').first,
-        :total_tracks => tag.frame_list('TRCK').first.to_s.split('/')[1],
-        :total_discs => disc[1],
-        :genre => tag.genre || old_tag.genre,
-        #:rating => ,
-        :bpm => tag.frame_list('TBPM').first,
-      )
-      t.filename = sinatra.relative_to filename
-      t.save!
-    }
-  end
-
-  def self.write_tags track
-    # Load an ID3v2 tag from a file
-    TagLib::MPEG::File.open("public/#{track.filename}") do |file|
-      tag = file.id3v2_tag
-
-      tag.title = track.title
-      tag.artist = track.artist
-      tag.album = track.album
-      tag.year = track.year.to_i
-      tag.genre = track.genre
-      # track count
-      if track.total_tracks and track.track
-        tag.frame_list('TRCK').first.text = "#{track.track}/#{track.total_tracks}"
-      elsif tag.track
-        tag.track = track.track.to_i
-      end
-      # add album artist
-      if track.albumartist
-        tag.add_frame TagLib::ID3v2::TextIdentificationFrame.new('TPE2',TagLib::String::UTF8) if tag.frame_list('TPE2').empty?
-        tag.frame_list('TPE2').first.text = track.albumartist
-      end
-      # add disc
-      if track.disc
-        tag.add_frame TagLib::ID3v2::TextIdentificationFrame.new('TPOS',TagLib::String::UTF8) if tag.frame_list('TPOS').empty?
-        disc = track.disc.to_s
-        disc << "/#{track.total_discs}" if track.total_discs
-        tag.frame_list('TPOS').first.text = disc
-      end
-      # bpm
-      if track.bpm
-        tag.add_frame TagLib::ID3v2::TextIdentificationFrame.new('TBPM',TagLib::String::UTF8) if tag.frame_list('TBPM').empty?
-        tag.frame_list('TBPM').first.text = track.bpm.to_s
-      end
-
-      file.save
-    end
-  end
-end
