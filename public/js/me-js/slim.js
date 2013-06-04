9d8852317f352c7147abdbe7bb3b8bdaef562328
Blaž Hrastnik <speed.the.bboy@gmail.com>
2013-06-04 18:59:46 +0200

    Remove the old play/pause plugin for the player

--- public/js/me-js/slim.js
+++ public/js/me-js/slim.js
@@ -41,7 +41,7 @@ if (typeof jQuery != 'undefined') {
     autosizeProgress : true,
 
     // features to show
-    features: ['playpause','current','duration','progress','volume'],
+    features: ['current','duration','progress','volume'],
 
     // turns keyboard support on and off for this instance
     enableKeyboard: true,
@@ -555,52 +555,6 @@ if (typeof jQuery != 'undefined') {
 
 })(mejs.$);
 
-(function($) {
-
-  $.extend(mejs.MepDefaults, {
-    playpauseText: 'Play/Pause'
-  });
-
-  // PLAY/pause BUTTON
-  $.extend(MediaElementPlayer.prototype, {
-    buildplaypause: function(player, controls, media) {
-      var
-        t = this,
-        play =
-        $('<div class="mejs-button mejs-playpause-button mejs-play" >' +
-          '<button type="button" aria-controls="' + t.id + '" title="' + t.options.playpauseText + '"></button>' +
-        '</div>')
-        .appendTo(controls)
-        .click(function(e) {
-          e.preventDefault();
-
-          if (media.paused) {
-            media.play();
-          } else {
-            media.pause();
-          }
-
-          return false;
-        });
-
-      media.addEventListener('play',function() {
-        play.removeClass('mejs-play').addClass('mejs-pause');
-      }, false);
-      media.addEventListener('playing',function() {
-        play.removeClass('mejs-play').addClass('mejs-pause');
-      }, false);
-
-
-      media.addEventListener('pause',function() {
-        play.removeClass('mejs-pause').addClass('mejs-play');
-      }, false);
-      media.addEventListener('paused',function() {
-        play.removeClass('mejs-pause').addClass('mejs-play');
-      }, false);
-    }
-  });
-
-})(mejs.$);
 (function($) {
 
   $.extend(mejs.MepDefaults, {
